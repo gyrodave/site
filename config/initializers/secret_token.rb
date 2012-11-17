a3acb10e81682fc625745df3c02c4cf6ed6dd09d
dave <gyrodave_fr@yahoo.fr>
2012-11-17 22:35:15 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lolosite::Application.config.secret_token = '2700daebf16fd5fa0536174d82d759a2e89c0c648944ce47eb6261888c1f77c3ac4486b9694227786ca78514d005c9e199557fe2c3ddc8dac66e1eb7e50e84a8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lolosite::Application.config.session_store :cookie_store, key: '_lolosite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lolosite::Application.config.session_store :active_record_store
